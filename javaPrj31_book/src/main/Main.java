d25816be26a853fcae0e314f1983f67e0383e50f
heejin-gaebal <pheejin603@gmail.com>
2025-04-26 23:42:55 +0900

    Add files via upload

---
+++ javaPrj31_book/src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("====== Book ======");
+		
+		BookManager bookManager = new BookManager();
+		bookManager.selectMenu();
+	}
+
+}
