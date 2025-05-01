b7085af1559a5e6b3ae8bb7e239b56ebce8c2b42
heejin-gaebal <pheejin603@gmail.com>
2025-05-01 17:52:43 +0900

    Add files via upload

---
+++ javaPrj01/src/main/Main.java
@@ -0,0 +1,26 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Main 파일입니다.");		
+		//	하나의 명령이 끝나면 ; 필수
+		
+		//	변수
+		int x = 10;
+		System.out.println( x );
+		
+		
+		
+		/*
+		 * 	= 은 대입하다라는 뜻
+		 *	int(integer)는 정수 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * */
+	}
+
+}
